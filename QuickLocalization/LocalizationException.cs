187ad20e4e4aaefc834402d05c2dd14e7a5ffc22
magicskysword <skyswordkill@qq.com>
2023-11-08 18:27:51 +0800

    添加QuickLocalization源码

---
+++ QuickLocalization/LocalizationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuickLocalization;
+
+public class LocalizationException : Exception
+{
+    public LocalizationException(string message) : base(message)
+    {
+    }
+    
+    public LocalizationException(string message,Exception e) : base(message, e)
+    {
+    }
+}
---
+++ QuickLocalization/LocalizationWarning.cs
@@ -0,0 +1,13 @@
+﻿namespace QuickLocalization;
+
+public class LocalizationWarning
+{
+    public LocalizationWarning(string fileName, string warning)
+    {
+        FileName = fileName;
+        Warning = warning;
+    }
+
+    public string FileName { get; set; }
+    public string Warning { get; set; }
+}
